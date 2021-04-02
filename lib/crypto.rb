07dfd9e85a87fd227f27f266a4f8e73fadb4331c
Jmacklin1 <Macklin.Jason@gmail.com>
2021-04-02 12:34:02 -0400

    first commit

--- lib/crypto.rb
+++
@@ -1,6 +0,0 @@
-require "crypto/version"
-
-module Crypto
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/environment.rb
@@ -0,0 +1,9 @@
+require_relative "crypto/version"
+require "pry"
+
+require "bundler"
+Bundler.require
+
+
+require_all './crypto'
+#binding.pry
